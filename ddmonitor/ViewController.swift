71a66fce33c80847b6d1f095c8ea11e8db09113b
huyuc0ng <850476264@qq.com>
2021-03-16 17:36:30 +0800

    Initial Commit

---
+++ ddmonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ddmonitor
+//
+//  Created by Cong Hu on 2021/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
